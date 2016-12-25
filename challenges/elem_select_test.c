e7de74aafb9066e032a85a8934c436084b0ed36a
Imed Eddine Nezzar <imad.eddine.nezzar@gmail.com>
2016-12-25 23:48:18 +0100

    updated elem_select arguments

--- challenges/elem_select_test.c
+++ challenges/elem_select_test.c
@@ -12,8 +12,8 @@ static int int_lesser(void* a, void* b) {
 int main(void) {
   int arr[] = {2, 9, 10, -2, 0, 1, 29, 3};
 
-  int* max = elem_select(arr, 8, sizeof(int), int_greater);
-  int* min = elem_select(arr, 8, sizeof(int), int_lesser);
+  int* max = elem_select(arr, arr + 8, sizeof(int), int_greater);
+  int* min = elem_select(arr, arr + 8, sizeof(int), int_lesser);
 
   printf("max: %d\n", *max);
   printf("min: %d\n", *min);
